2677003a587913a64acdaa12219d33010eb27d71
evenegas3 <evenegas3@users.noreply.github.com>
2020-01-17 15:22:20 -0700

    UI Complete
    Main UI is complete, needs onclick implemented

---
+++ projects/calculator/ista330.sp20.calculator.api/service.js
@@ -0,0 +1,21 @@
+// Erick Venegas
+
+"use strict";
+(function(){
+
+    window.onload = function(){
+        document.getElementById("calculate").onclick = startbutton();
+    };
+
+    /**
+    * Function is called when start button is pressed by user.    *
+    * @returns {None}
+    */
+    function startbutton(){
+        info = document.getElementById("expression")
+
+    }
+
+
+
+})();
