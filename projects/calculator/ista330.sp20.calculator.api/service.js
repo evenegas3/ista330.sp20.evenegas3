19427ef63caad2b6161c8450b6be4ce151f8350d
evenegas3 <evenegas3@users.noreply.github.com>
2020-01-21 11:42:50 -0700

    Server test via node command
    Unable to do pdf commands, thus trying to do it with node command

--- projects/calculator/ista330.sp20.calculator.api/service.js
+++ projects/calculator/ista330.sp20.calculator.api/service.js
@@ -1,18 +1,43 @@
-// dependencies
-const express = require(’express’);
-const url = require(’url’);
+// const express = require(’express’);
+// const url = require(’url’);
+// const app = express();
 
-//create the server 
+// app.get(’/’, (request , response) => {
+// 	var urlParts = url.parse(request.url, true);
+// 	var parameters = urlParts.query;
+// 	var expression = parameters.expression;
+// 	response.send(expression + " = ?");
+// });
+
+// app.listen(3001);
+
+// Erick Venegas
+
+const express = require("express");
 const app = express();
-const port = 3001;
+const fs = require("fs");
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+console.log('web service started');
 
-// the methods
-app.get(’/’, (request , response) => {
-var urlParts = url.parse(request.url, true);
-var parameters = urlParts.query;
-var expression = parameters.expression;
-response.send(expression + " = ?");
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", 
+               "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
-// start the server
-app.listen(port, ()=> console.log (’Listening on port’ + port));
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+	res.header("Access-Control-Allow-Origin", "*");
+	console.log(req);
+	console.log(res);
+	// let information = getFileInfo();
+	// res.send(JSON.stringify(information));
+})
+
+
+
+app.listen(3001);
