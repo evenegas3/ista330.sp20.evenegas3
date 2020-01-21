89ed08318180effa26c409ba2bc1c0cf8addcf60
evenegas3 <evenegas3@users.noreply.github.com>
2020-01-21 14:27:19 -0700

    Final Version
    Completed assignment

--- projects/calculator/ista330.sp20.calculator.api/service.js
+++ projects/calculator/ista330.sp20.calculator.api/service.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // dependencies
 const express = require(’express’);
 const url = require(’url’);
@@ -16,3 +17,22 @@ response.send(expression + " = ?");
 
 // start the server
 app.listen(port, ()=> console.log (’Listening on port’ + port));
+=======
+// Erick Venegas
+// ISTA330
+// 01-21-20
+// service.js
+
+const express = require('express');
+const cors = require("cors")
+const url = require('url');
+const app = express();
+const port = 3001;
+
+app.get('/', cors(), (request , response) => {
+	var expression = request.url.split("=")[1];
+	response.json({message: expression + " = ?"});
+});
+
+app.listen(port, ()=> console.log('Listening on port ' + port));
+>>>>>>> Stashed changes
